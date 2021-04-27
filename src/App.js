845e84e0a4e316acd3d4fa117924f66d85dd51bc
adeha047 <dehaven7777@gmail.com>
2021-04-26 21:36:55 -0400

    added new components files and updated app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,80 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Searchbar from './components/input.js';
+import API from './utils/API.js'
+import EmployeeTable from './components/table.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Searchbar />
 
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <Searchbar />
+
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // error: null,
+      // isLoaded: false,
+      employees: []
+    };
+  }
+
+  componentDidMount() {
+    API.getUsers()
+      // .then(res => res.json())
+      .then(
+        (result) => {
+          // console.log(result.data.results)
+          this.setState({
+            employees: result.data.results
+          })
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
+
+  render() {
+    //   if (error) {
+    //     return <div>Error: {error.message}</div>;
+    //   } else if (!isLoaded) {
+    //     return <div>Loading...</div>;
+    //   } else {
+    //     return (
+    //       <ul>
+    //         {items.map(item => (
+    //           <li key={item.id}>
+    //             {item.name} {item.price}
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     );
+    //   }
+    // }
+    return (
+      <div> 
+        {this.state.employees ? (
+          <div> <Searchbar />
+          <EmployeeTable employees={this.state.employees} /> </div>
+          
+
+        ): <h1> Loading... </h1>}
+        
+
+      </div>
+    )
+  }
+
 }
 
 export default App;
