faf4612c5757918aacb5b4fd6c57ff52e53c3c52
adeha047 <dehaven7777@gmail.com>
2021-04-27 09:05:28 -0400

    added new button

--- src/components/input.js
+++ src/components/input.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
 
 const Searchbar = () => {
     return (
+        <>
         <Form>
-            <Form.Control placeholder="First name" />
+            <Form.Control placeholder="Enter Emplyoyee Name" />
         </Form>
+        <button className="btn btn-primary" onClick={this.ComponentDidMount}>
+        Submit
+      </button>
+      </>
+        
+         
 
     )
 };
