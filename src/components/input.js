d3168d2aeadec51f35d206a41f5f5096bcfe1400
adeha047 <dehaven7777@gmail.com>
2021-04-26 20:48:53 -0400

    added new bootstrap

---
+++ src/components/input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+const Searchbar = () => {
+    return (
+        <Form>
+            <Form.Control placeholder="First name" />
+        </Form>
+
+    )
+};
+
+export default Searchbar;
