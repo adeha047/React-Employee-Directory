257205b0f42bb691f298e3f5f94ac6708d52c453
adeha047 <dehaven7777@gmail.com>
2021-04-28 20:44:24 -0400

    added new files

--- src/components/employees/table.js
+++ src/components/employees/table.js
@@ -9,13 +9,13 @@ const EmployeeTable = (props) => {
         {props.employees.map(employee => (
           <ul>
             <li className="list-group-item" >
-            <span className="remove" onClick={() => employees.handleRemove(employees.id)}>𝘅</span>
               {employee.email}
               <div className="img-container"></div>
               <img alt={employee.name} className="img-fluid" src={employee.picture.large} />
               {employee.name.first}
               {employee.name.last}
             </li>
+            <span className="remove" onClick={() => props.handleRemove(props.id)}>X</span>
           </ul>
         ))}
       </div>
@@ -24,28 +24,5 @@ const EmployeeTable = (props) => {
   )
 }
 
-// function FriendCard(props) {
-//   return (
-//     <div className="card">
-//       <div className="img-container">
-//         <img alt={props.name} src={props.image} />
-//       </div>
-//       <div className="content">
-//         <ul>
-//           <li>
-//             <strong>Name:</strong> {props.name}
-//           </li>
-//           <li>
-//             <strong>Occupation:</strong> {props.occupation}
-//           </li>
-//           <li>
-//             <strong>Address:</strong> {props.location}
-//           </li>
-//         </ul>
-//       </div>
-//       <span className="remove" onClick={() => props.handleRemove(props.id)}>𝘅</span>
-//     </div>
-//   );
-// }
 
 export default EmployeeTable;
